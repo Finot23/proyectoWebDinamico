65c20d16c33b008eb2dab29495fe8834eb8771b6
Finot23 <luis.ridr78@gmail.com>
2025-12-08 00:44:44 -0600

    Se agrego ejemplos para el post de swagger

--- backend/inventory_service/src/main/java/mx/uam/model/dto/CategoriaDTO.java
+++ backend/inventory_service/src/main/java/mx/uam/model/dto/CategoriaDTO.java
@@ -1,14 +1,13 @@
 package mx.uam.model.dto;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public class CategoriaDTO {
-    /* hay que cambiar esto porque no coincide en el post.
-    private Integer id;
-    private String nombre;
-    private String direccion;
-*/
-
+   
+@Schema(description = "ID autogenerado", accessMode = Schema.AccessMode.READ_ONLY)
    private Integer id;
+@Schema(description = "Breve descripcion de la categoria", example = "Gadgets tecnologicos")
    private String descripcion;
+@Schema(description = "Nombre de la categoria", example = "Juegos de Mesa")
    private String nombre;
 
     public Integer getId() {
