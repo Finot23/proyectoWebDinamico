2fb0090f46d785ade55debd5bee340a29ed3805a
Finot23 <luis.ridr78@gmail.com>
2025-12-08 00:33:45 -0600

    Ya se arreglo el problema de producto pero toca tambien modificar el diagrama de SQL para que tenga logica con lo construido

--- backend/inventory_service/src/main/java/mx/uam/model/entity/Producto.java
+++ backend/inventory_service/src/main/java/mx/uam/model/entity/Producto.java
@@ -1,73 +1,90 @@
 package mx.uam.model.entity;
 
+import jakarta.persistence.CascadeType; 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;    
+import java.util.ArrayList;              
+import java.util.List;                   
 
 @Entity
 public class Producto {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
-	private String nombre;
-	private String descripcion;
-	private double precio;
-	private Integer stockActual;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nombre;
+    private String descripcion;
+    private double precio;
+    private Integer stockActual;
 
-	@ManyToOne
-	@JoinColumn(name = "categoria_id")
-	private Categoria categoria;
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
+
+ 
+    
+    @OneToMany(mappedBy = "producto", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<StockMovement> movimientos = new ArrayList<>();
+   
 
     public Integer getId() {
         return id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public double getPrecio() {
-        return precio;
-    }
-
-    public Integer getStockActual() {
-        return stockActual;
-    }
-
-    public Categoria getCategoria() {
-        return categoria;
     }
 
     public void setId(Integer id) {
         this.id = id;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
     }
 
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
 
+    public double getPrecio() {
+        return precio;
+    }
+
     public void setPrecio(double precio) {
         this.precio = precio;
+    }
+
+    public Integer getStockActual() {
+        return stockActual;
     }
 
     public void setStockActual(Integer stockActual) {
         this.stockActual = stockActual;
     }
 
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
     public void setCategoria(Categoria categoria) {
         this.categoria = categoria;
     }
 
-	
+    
+    public List<StockMovement> getMovimientos() {
+        return movimientos;
+    }
+
+    public void setMovimientos(List<StockMovement> movimientos) {
+        this.movimientos = movimientos;
+    }
 }
